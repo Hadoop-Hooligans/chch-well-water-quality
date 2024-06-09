7baafbf7d27dbe3b764197fd600f2c54df1cbc41
Naman Rastogi <nra99@uclive.ac.nz>
2024-06-09 09:12:24 +0000

    Adds a test for databse connection

--- src/db.js
+++ src/db.js
@@ -16,3 +16,16 @@ console.log(process.env)
 module.exports = {
     query: (text, params) => pool.query(text, params)
 };
+// Example usage
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release();
+        if (err) {
+            return console.error('Error executing query', err.stack);
+        }
+        console.log(result.rows);
+    });
+});
